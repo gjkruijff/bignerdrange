22c3ef47a881400bb3bb7b12b362239d9bba4011
Kruijff, Geert-Jan <gj.kruijff@gmail.com>
2016-06-11 13:23:06 +0200

    asset file, changed employee file

---
+++ BMITime2/BMITime2/Asset.h
@@ -0,0 +1,13 @@
+//
+//  Asset.h
+//  BMITime2
+//
+//  Created by Geert-Jan Kruijff on 11/06/16.
+//  Copyright © 2016 Geert-Jan Kruijff. All rights reserved.
+//
+
+#ifndef Asset_h
+#define Asset_h
+
+
+#endif /* Asset_h */
---
+++ BMITime2/BMITime2/Asset.m
@@ -0,0 +1,9 @@
+//
+//  Asset.m
+//  BMITime2
+//
+//  Created by Geert-Jan Kruijff on 11/06/16.
+//  Copyright © 2016 Geert-Jan Kruijff. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
