e4f2be91a8b2e6d37b00a8c3f8ea6abadd527f9d
Kruijff, Geert-Jan <gj.kruijff@gmail.com>
2016-06-12 16:20:55 +0200

    main with defined macro and NSLocale dictionary access

---
+++ Constants/Constants/main.m
@@ -0,0 +1,31 @@
+//
+//  main.m
+//  Constants
+//
+//  Created by Geert-Jan Kruijff on 12/06/16.
+//  Copyright © 2016 Geert-Jan Kruijff. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, Constants World!");
+        
+        // In literal NSString, use \u for unicode characters
+        NSLog(@"\u03c0 is %f", M_PI);
+        
+        // Next, use MAX -- MAX is a #define'd macro -- a ternary statement, i.e. not a function
+        NSLog(@"%d is larger", MAX(10,12));
+        
+        // Use the user's locale
+        NSLocale *here = [NSLocale currentLocale];
+        NSString *currency = [here objectForKey:@"currency"];
+        NSLog(@"Money is %@", currency);
+        
+        
+        
+    }
+    return 0;
+}
